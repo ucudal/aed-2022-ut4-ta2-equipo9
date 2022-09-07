185726217341e5845789e6832ce0dc20558ac2b1
Cdogonza <Cdogonzagonzalo.ucu@correo.ucu.edu.uy>
2022-09-07 19:39:03 -0300

    Metodo Insertar

--- UT4-TA/src/TElementoAB.java
+++ UT4-TA/src/TElementoAB.java
@@ -4,7 +4,7 @@ public class TElementoAB<T> implements IElementoAB<T> {
     private TElementoAB hijoIzq;
     private TElementoAB hijoDer;
     private T datos;
-
+    private int contador =0;
     /**
      * @param unaEtiqueta
      * @param unosDatos
@@ -14,6 +14,90 @@ public class TElementoAB<T> implements IElementoAB<T> {
     public TElementoAB(Comparable unaEtiqueta, T unosDatos) {
         etiqueta = unaEtiqueta;
         datos = unosDatos;
+    }
+
+    public int getContador(){
+        return this.contador;
+    }
+    
+    @Override
+    public Comparable getEtiqueta() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public TElementoAB<T> getHijoIzq() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public TElementoAB<T> getHijoDer() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setHijoIzq(TElementoAB<T> elemento) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setHijoDer(TElementoAB<T> elemento) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public TElementoAB<T> buscar(Comparable unaEtiqueta) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean insertar(TElementoAB<T> elemento) {
+        this.contador+=1;
+        System.out.println(this.contador+"contador");
+        if(this.etiqueta == elemento.etiqueta){
+            return true;
+        }
+        if (elemento.etiqueta.compareTo(this.etiqueta)<0){
+            if(this.hijoIzq==null){
+                this.hijoIzq=elemento;
+            }else{
+                hijoIzq.insertar(elemento);
+                return true;
+            }
+        }else{
+            if(this.hijoDer==null){
+                this.hijoDer=elemento;
+            }else{
+                hijoDer.insertar(elemento);
+                return true;
+            }
+        }
+          return false; 
+    }
+
+    @Override
+    public String preOrden() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String inOrden() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String postOrden() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public T getDatos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public TElementoAB eliminar(Comparable unaEtiqueta) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
    	
