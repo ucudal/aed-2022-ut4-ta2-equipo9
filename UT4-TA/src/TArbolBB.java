ec2b2c566e8aaca67783e74c51f8950d4a6ec394
Sebastian Olivera <88690503+SebastianOlivera@users.noreply.github.com>
2022-09-07 18:55:43 -0300

    Cambios de gonza

---
+++ UT4-TA/src/TArbolBB.java
@@ -0,0 +1,44 @@
+public class TArbolBB<T> implements IArbolBB<T> {
+
+    private TElementoAB<T> raiz;
+
+    /**
+     * Separador utilizado entre elemento y elemento al imprimir la lista
+     */
+    public static final String SEPARADOR_ELEMENTOS_IMPRESOS = "-";
+
+    public TArbolBB() {
+        raiz = null;
+    }
+
+    @Override
+    public boolean insertar(TElementoAB<T> unElemento) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public TElementoAB<T> buscar(Comparable unaEtiqueta) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String preOrden() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String inOrden() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String postOrden() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminar(Comparable unaEtiqueta) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+       }
